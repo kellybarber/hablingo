24e67408023ff024d8e771153897cc71064e62d8
Kelly Barber <Kelly@Kellys-MacBook-Air.local>
2019-03-27 11:27:40 -0600

    # Added Home section to replace dashboard

--- src/routers/App.js
+++ src/routers/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { BrowserRouter, Switch } from 'react-router-dom'
 import { Route } from 'react-router-dom'
-import Dashboard from '../components/Dashboard/Dashboard'
+import Home from '../sections/Home/Home'
 
 export default () => (
   <BrowserRouter>
     <div class='app-container'>
       <Switch>
-        <Route path='/' component={Dashboard}/>
+        <Route path='/' component={Home}/>
       </Switch>
     </div>
   </BrowserRouter>
--- src/sections/Home/Home.js
+++ src/sections/Home/Home.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default () => (
   <div>
-    This is a protected route
+    Home
   </div>
 )
